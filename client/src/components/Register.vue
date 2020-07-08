18d19877fc2107ce39e38a996b8be6274e261126
Suneet Jena <suneetjena@Suneets-MacBook-Air.local>
2020-07-08 23:14:47 +0530

    Fullstack Setup

---
+++ client/src/components/Register.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>Register {{name}}.app</h1>
+    <input type="email" placeholder="E-Mail" v-model="email" />
+    <br />
+    <input type="password" placeholder="Password" v-model="password" />
+    <br />
+    <input type="button" value="Register" @click="register" />
+  </div>
+</template>
+
+<script>
+import auth from "../services/AuthenticationService";
+export default {
+  data() {
+    return {
+      name: "Suneet",
+      email: "",
+      password: ""
+    };
+  },
+  methods: {
+    async register() {
+      await auth.register({
+        email: this.email,
+        password: this.password
+      });
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
--- server/src/app.js
+++ server/src/app.js
@@ -8,9 +8,9 @@ app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/status', (req, res) => {
+app.post('/register', (req, res) => {
     res.send({
-        message: 'Cool'
+        message: `The mail id is ${req.body.email}`
     })
 })
 
